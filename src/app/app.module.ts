866f9b65726eb220c23e9f8abcab8ef3d5ba1192
Sergey Voitenko <sergey.voitenko98@gmail.com>
2020-12-24 18:06:51 +0200

    Create Main page and Contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContactsComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: MainComponent
+      },
+      {
+        path: 'contacts',
+        component: ContactsComponent
+      },
+      {
+        path: '**',
+        component: NotfoundComponent
+      }
+    ]),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCo4AXMsSGbfUbKqhjKrt1MscUbOHagrBk'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
