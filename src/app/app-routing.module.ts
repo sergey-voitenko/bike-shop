e1ba5f29886fdd03113917af414f2fce57b8a507
Sergey Voitenko <sergey.voitenko98@gmail.com>
2021-01-13 21:14:09 +0200

    Enhance forms and apply validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import {ContactsComponent} from './components/contacts/contacts.component';
 import {ProductComponent} from './components/product/product.component';
 import {NotfoundComponent} from './components/notfound/notfound.component';
 import {ReviewsComponent} from './components/reviews/reviews.component';
+import {ContactsSuccessComponent} from './components/contacts-success/contacts-success.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'contacts', component: ContactsComponent},
+  {path: 'contacts/success', component: ContactsSuccessComponent},
   {path: 'product/:id', component: ProductComponent},
   {path: 'reviews/:id', component: ReviewsComponent},
   {path: 'order', loadChildren: () => import('src/app/modules/order.module').then(m => m.OrderModule)},
